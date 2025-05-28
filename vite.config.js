81b457eee1f8371eb00d6f2ab58aeed2b0267cb7
Abdaziz <dev.abdaziz@outlook.com>
2025-05-28 14:26:57 +0700

    poc finish fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8080', // ganti dengan URL backend
+//         changeOrigin: true,
+        
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // ganti dengan URL backend
-        changeOrigin: true,
-        
-      }
-    }
-  }
 })
